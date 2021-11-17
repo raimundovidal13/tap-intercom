535a8a7954ed26ae11fbf946ebb4f22442fd5088
Vidal <79987950+raimundovidal13@users.noreply.github.com>
2021-11-17 14:46:39 -0300

    Update discover.py

--- tap_intercom/discover.py
+++ tap_intercom/discover.py
@@ -32,7 +32,7 @@ def discover():
     for schema_name, schema in schemas.items():
         schema_meta = field_metadata[schema_name]
 
-        if schema_name in ['companies', 'segments', 'admins']:
+        if schema_name in ['contacts']:
             schema_meta[0]["metadata"]["selected"] = True
                 
         catalog_entry = {
