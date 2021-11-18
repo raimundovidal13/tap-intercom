2555bfeb3bdfd523b5fc7c31f43494b41c37936f
Vidal <79987950+raimundovidal13@users.noreply.github.com>
2021-11-18 13:52:06 -0300

    reducing amount of requests per 10 s

--- tap_intercom/client.py
+++ tap_intercom/client.py
@@ -153,7 +153,8 @@ class IntercomClient(object):
                           (Server5xxError, ConnectionError, Server429Error),
                           max_tries=7,
                           factor=3)
-    @utils.ratelimit(1000, 60)
+    #@utils.ratelimit(1000, 60)
+    @utils.ratelimit(10, 10)
     def check_access_token(self):
         if self.__access_token is None:
             raise Exception('Error: Missing access_token.')
@@ -182,7 +183,8 @@ class IntercomClient(object):
                           (Server5xxError, ConnectionError, Server429Error, IntercomScrollExistsError),
                           max_tries=7,
                           factor=3)
-    @utils.ratelimit(1000, 60)
+    #@utils.ratelimit(1000, 60)
+    @utils.ratelimit(10, 10)
     def request(self, method, path=None, url=None, **kwargs):
         if not self.__verified:
             self.__verified = self.check_access_token()
